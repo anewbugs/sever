251b7df10b0a933aa7e21642476d3d3dce7a9913
anewbugs <876573435@qq.com>
2020-04-05 23:23:41 +0800

    基础

---
+++ src/main/java/core/ob/Observer.java
@@ -0,0 +1,15 @@
+package core.ob;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+/**
+ * @author wu
+ * 观察者模式
+ * 主要用于消息的分发
+ */
+public class Observer {
+
+    private HashMap<String,Method> ob = new HashMap<>();
+
+}
