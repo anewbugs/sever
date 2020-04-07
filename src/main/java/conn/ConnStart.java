48c3f729de1163889e623a8836a84e59f8ffb348
Wu <876573435@qq.com>
2020-04-07 17:10:01 +0800

    连接服务启动

--- src/main/java/conn/ConnStart.java
+++ src/main/java/conn/ConnStart.java
@@ -1,9 +1,19 @@
 package conn;
 
+import core.boot.config.Config;
+import core.thread.Department;
+import core.thread.Headquarters;
+
+/**
+ * 有关连接服务启动
+ */
 public class ConnStart {
 
-    public void init(){
-
+    public void init(Headquarters head){
+        for (int i = 0; i < Config.DEPART_CONN_SIZE; i++) {
+            Department depart = new ConnDepart(Config.DEPART_CONN_NAME + i);
+           depart.start(head);
+        }
     }
 
 }
