182e7ac31116a0e7eadb317b8dacce052afefac3
Wu <876573435@qq.com>
2020-04-09 11:36:56 +0800

    网络

---
+++ src/main/java/conn/netty/Net.java
@@ -0,0 +1,82 @@
+package conn.netty;
+
+import java.util.concurrent.TimeUnit;
+
+import core.boot.config.Config;
+import core.until.LogUntil;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+public class Net extends Thread{
+    @Override
+    public void run() {
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+
+        serverBootstrap
+                .group(boosGroup,workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.TCP_NODELAY,true)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+
+
+                    protected void initChannel(NioSocketChannel nioSocketChannel) {
+//                        //连接处理Handler
+//                        nioSocketChannel.pipeline().addLast("ConnectionHandler",new ConnectionHandler());
+//                        //消息长度处里Handler 解决半包和粘包问题
+//                        nioSocketChannel.pipeline().addLast("LengthFieldBasedFrameDecoder", new LengthFieldBasedFrameDecoder(MAX_FRAME_LENGTH,LENGTH_FIELD_OFFSET,LENGTH_FIELD_LENGTH,LENGTH_ADJUSTMENT,INITIAL_BYTES_TO_STRIP));
+//                        //数据封包处理Handler
+//                        nioSocketChannel.pipeline().addLast("LengthFieldPrepender",new LengthFieldPrepender(LENGTH_FIELD_LENGTH));
+//                        //超时控制handler
+//                        nioSocketChannel.pipeline().addLast("ReadTimeoutHandler" , new ReadTimeoutHandler(CONNECTION_TIMEOUT_SECOND));
+//                        //服务器断连
+//                        nioSocketChannel.pipeline().addLast("ServerHandler" ,new ServerHandler());
+//                        //Ping消息回应
+//                        nioSocketChannel.pipeline().addLast("PingHandle", new PingHandle());
+//                        //登入消息处理
+//                        nioSocketChannel.pipeline().addLast("LoginHandle" , new LoginHandle());
+//                        //玩家数据获取
+//                        nioSocketChannel.pipeline().addLast("UserHandle",new UserHandle());
+//                        //房间消息管理
+//                        nioSocketChannel.pipeline().addLast("RoomHandle" , new RoomHandle());
+
+
+                    }
+                });
+        bind(serverBootstrap, Config.LOCAL_PORT);
+        LogUntil.logger.info("网络服务启动*********************************");
+
+
+//        while(!Thread.interrupted()){
+//            try {
+//                TimeUnit.MILLISECONDS.sleep(NETTY_SHUTDOWN_CHECKED_PERIOD);
+//            } catch (InterruptedException e) {
+//                LogUntil.logger.error(e.toString());
+//                Thread.currentThread().interrupt();
+//            }
+//        }
+//        LogUntil.logger.info("netty关闭");
+//        boosGroup.shutdownGracefully();
+//        workerGroup.shutdownGracefully();
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                LogUntil.logger.info("端口[" + port + "]绑定成功!");
+            } else {
+                LogUntil.logger.info("端口[" + port + "]绑定失败!");
+            }
+        });
+    }
+}
